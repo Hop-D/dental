6d426fc687d3c1d48383991ea9ef9bdab6d45383
Hop-D <95290625+Hop-D@users.noreply.github.com>
2023-04-23 17:19:43 +0800

    Update Main.java

--- dental/src/dental/Main.java
+++ dental/src/dental/Main.java
@@ -1,10 +1,12 @@
+
 package dental;
 
 import java.util.ArrayList;
 
 public class Main {
-	
-	static ArrayList <User> users = new ArrayList<>();
+
+    
+    static ArrayList <User> users = new ArrayList<>();
     static ArrayList <Dentist> dentists = new ArrayList<>();
     static ArrayList <Message> messages = new ArrayList<>();
     static ArrayList <Appointment> appointments = new ArrayList<>();
@@ -20,22 +22,23 @@ public class Main {
         users.add(new Dentist(5, "Dentist Two", "d2@email.com", "pass", "Dentist"));
         dentists.add(new Dentist(2, "Dentist Two", "d2@email.com", "pass", "Dentist"));
         
-        messages.add(new Message(1, "test", "test", "test"));
-        messages.add(new Message(2, "test2", "test2", "test2"));
+        messages.add(new Message(1, "test", "test", "Dentist One"));
+        messages.add(new Message(2, "test2", "test2", "Dentist Two"));
         
-        appointments.add(new Appointment(1, "nyok", "nyok", "1234567890", "lol", "lol", "lol", "Dentist One", "Waiting"));
-        appointments.add(new Appointment(2, "test", "test", "1234567890", "lol", "lol", "lol", "Dentist Two", "Waiting"));
+        appointments.add(new Appointment(1, "nyok", "nyok", "1234567890", "Sun Apr 23 15:52:57 PST 2023", "Sun Apr 23 15:52:57 PST 2023", "lol", "Dentist One", "Waiting"));
+        appointments.add(new Appointment(2, "test", "test", "1234567890", "Sun Apr 23 15:52:57 PST 2023", "Sun Apr 23 15:52:57 PST 2023", "lol", "Dentist Two", "Waiting"));
+        appointments.add(new Appointment(3, "nyok", "nyok", "1234567890", "Sun Apr 22 15:52:57 PST 2023", "Sun Apr 24 15:52:57 PST 2023", "lol", "Dentist One", "APPROVED"));
+        appointments.add(new Appointment(4, "test", "test", "1234567890", "Sun Apr 22 15:52:57 PST 2023", "Sun Apr 24 15:52:57 PST 2023", "lol", "Dentist Two", "APPROVED"));
     }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Main main = new Main();
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main main = new Main();
         WelcomeClass welcome = new WelcomeClass();
         welcome.setVisible(true);
         welcome.pack();
         welcome.setLocationRelativeTo(null);
 
-	}
-
+    }
+    
 }
